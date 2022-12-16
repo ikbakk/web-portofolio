8f0d7b19764bfce1322a154ed84e99d6b7a85895
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-17 01:04:57 +0800

    Resize photocard

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PhotoCard = ({ url }) => {
   return (
-    <div className="flex h-4/6 w-4/6 ">
+    <div className="flex basis-1/2">
       <div className="h-full w-full overflow-hidden shadow-offset9tl shadow-fadeBlack outline outline-2 outline-fadeBlack">
         <img className="" src={url} />
       </div>
