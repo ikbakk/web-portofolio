ceeb0d0f8f1830abcb48409c50e36f1975a410e6
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-17 13:45:44 +0800

    testing

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,21 +5,9 @@ import HomeHeader from "../HomeHeader";
 const Home = () => {
   const name = "MUHAMMAD IQBAL FIRDAUS";
   return (
-    <div className="relative flex h-screen flex-col pt-10">
-      <div className="relative flex flex-row bg-gradient-to-bl from-paleSkin to-palePink outline outline-2">
-        {/* LeftSide */}
-        <div className="flex w-48 shrink-0 basis-5 items-center justify-center px-2">
-          <SideIcons />
-        </div>
-        {/* LeftSide */}
-        {/* Mid */}
-        <div className="flex basis-90 outline outline-2 outline-fadeBlack">
-          <HomeHeader name={name} />
-        </div>
-        {/* Mid */}
-        {/* RightSide */}
-        <div className="flex shrink-0 basis-5"></div>
-        {/* RightSide */}
+    <div className={`flex h-screen w-screen flex-col`}>
+      <div className="h-full bg-gradient-to-bl from-paleSkin to-palePink lg:h-full">
+        <HomeHeader name={name} />
       </div>
     </div>
   );
--- src/components/HomeHeader/index.jsx
+++ src/components/HomeHeader/index.jsx
@@ -5,8 +5,8 @@ import PhotoCard from "../PhotoCard";
 
 const HomeHeader = ({ name }) => {
   return (
-    <div className="m-8 flex basis-full flex-wrap space-y-7 p-8 lg:flex-nowrap xl:flex-nowrap">
-      <div className="flex basis-full flex-col space-y-4 ">
+    <div className="m-8 flex flex-col space-y-7 p-8 lg:flex-row">
+      <div className="flex basis-1/2 flex-col space-y-4 bg-white">
         <h1 className="mt-5 px-8 text-left font-syncopate text-6xl font-extrabold text-primaryDark duration-300 md:text-6xl lg:text-8xl">
           HI, IM<br></br>
           {name}
@@ -19,11 +19,11 @@ const HomeHeader = ({ name }) => {
           since July 2022 when try to combine IoT and Web development because of
           project for engineering degree.
         </p>
-        <div className="flex px-10 py-4">
+        <div className="flex justify-center px-10 py-4 lg:justify-start">
           <ButtonIvert text="DOWNLOAD CV" />
         </div>
       </div>
-      <div className=" flex basis-full items-center justify-center p-2">
+      <div className="flex  items-center justify-center p-2">
         <PhotoCard url="https://www.w3schools.com/w3css/img_avatar3.png" />
       </div>
     </div>
